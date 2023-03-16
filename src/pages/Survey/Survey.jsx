7b925b3a6cb3a04bac454932ef8be305c831927e
bgonzalez2022 <brunogarcia_673@hotmail.com>
2023-03-16 17:21:24 +0100

    correction des warning sur style components

--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -1,37 +1,61 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { useParams, Link } from "react-router-dom";
 import { Loader } from "../../utils/Atom";
 import styled from "styled-components";
 
-//
+const LoaderStyled = styled.div`
+  margin: auto;
+  width: fit-content;
+  padding: 10px;
+  display: block;
+`;
+const QuestionStyled = styled.h3`
+  height: 50px;
+  margin-bottow: 10px;
+`;
+
 function Survey() {
   const [surveyData, setSurveyData] = useState();
   const [dataLoading, setDataLoading] = useState(false);
-
-  const LoaderStyled = styled.div`
-    margin: auto;
-    width: fit-content;
-  `;
-
+  const nbQuestions = useRef(0);
   useEffect(() => {
     fetch("http://localhost:8000/survey")
       .then((response) => response.json())
       .then(({ surveyData }) => {
         setSurveyData(surveyData);
+        nbQuestions.current = Object.keys(surveyData).length;
         setDataLoading(true);
-        console.log(surveyData[1]);
       })
       .catch((error) => console.log(error));
   }, []);
 
   const { questionNum } = useParams();
-  const questionNumInt = parseInt(questionNum);
+  let questionNumInt = parseInt(questionNum);
   const questionNumPrecedente = questionNumInt > 1 ? questionNumInt - 1 : 1;
-  const questionNumSuivante =
-    questionNumInt > 0 ? (questionNumInt < 10 ? questionNumInt + 1 : 10) : 1;
+  let questionNumSuivante = 0;
+  if (questionNumInt > 0 && questionNumInt < nbQuestions.current) {
+    questionNumSuivante = questionNumInt + 1;
+  } else {
+    questionNumSuivante = nbQuestions.current;
+  }
+
   return (
     <div>
+      <div>
+        <h1>Questionnaire</h1>
+        <LoaderStyled>
+          {dataLoading ? (
+            <div>
+              <h2>
+                Question {questionNum} / {nbQuestions.current}
+              </h2>
+              <QuestionStyled>{surveyData[questionNumInt]}</QuestionStyled>
+            </div>
+          ) : (
+            <Loader />
+          )}
+        </LoaderStyled>
+      </div>
       {questionNumInt === 1 ? (
         <Link>
           <button disabled>Précedente</button>
@@ -42,7 +66,7 @@ function Survey() {
         </Link>
       )}
 
-      {questionNumInt === 10 ? (
+      {questionNumInt === nbQuestions.current ? (
         <Link to="/results">
           <button>Results</button>
         </Link>
@@ -51,14 +75,6 @@ function Survey() {
           <button>Suivante</button>
         </Link>
       )}
-
-      <div>
-        <h1>Questionnaire</h1>
-        <h2>Question {questionNum}</h2>
-        <LoaderStyled>
-          {dataLoading ? <h3>{surveyData[questionNumInt]}</h3> : <Loader />}
-        </LoaderStyled>
-      </div>
     </div>
   );
 }
